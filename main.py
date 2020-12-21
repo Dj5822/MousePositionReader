9d74d9f56427965ac98d6874a3c707ce65be366e
dj5822 <dj5822@gmail.com>
2020-12-21 22:25:04 +1300

    Now shows the RGB value of the pixel at mouse position.

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ try:
     previous_y = -1
     while True:
         x, y = pyautogui.position()
-        positionString = "x:" + str(x).rjust(4) + " y:" + str(y).rjust(4)
+        position_string = "x:" + str(x).rjust(4) + " y:" + str(y).rjust(4)
 
         # updates the output if the position changes.
         if previous_x != x or previous_y != y:
-            print("\b" * len(positionString), end="", flush=True)
-            print(positionString, end="")
+            pixel_color = pyautogui.screenshot().getpixel((x, y))
+            position_string += " RGB: (" + str(pixel_color[0]).rjust(3) + ", " + \
+                               str(pixel_color[1]).rjust(3) + ", " + str(pixel_color[2]).rjust(3) + ")"
+            print("\b" * len(position_string), end="", flush=True)
+            print(position_string, end="")
             previous_x = x
             previous_y = y
 except KeyboardInterrupt:
